94f02628b945d5acaeb97d41a8eca271587fff9d
Anatsrisaputh <jane.stbc@gmail.com>
2020-04-08 23:11:30 +0700

    Update learn javascript

---
+++ javascript/manipulate-arrays-with-unshift.js
@@ -0,0 +1,9 @@
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+console.log(myArray);
+// Only change code below this line
+
+//Add ["Paul",35] to the beginning of the myArray variable using unshift().
+myArray.unshift(["Paul",35]);
+console.log(myArray);
---
+++ javascript/shopping-list.js
@@ -0,0 +1,8 @@
+var myList = [["cat", 5], ["dog", 3], ["chicken", 1], ["pig", 1], ["tiger", 2], ["lion", 8]];
+
+// 1.myList should be an array.
+// 2.The first elements in each of your sub-arrays should all be strings.
+// 3.The second elements in each of your sub-arrays should all be numbers.
+// 4.You should have at least 5 items in your list.
+
+console.log(myList);
