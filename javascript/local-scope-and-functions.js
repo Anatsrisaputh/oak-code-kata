f3bed79d8942e5ca48098be5256b1ea92c76552e
Anatsrisaputh <jane.stbc@gmail.com>
2020-04-09 23:52:34 +0700

    Update homework

---
+++ javascript/local-scope-and-functions.js
@@ -0,0 +1,19 @@
+/*Declare a local variable myVar inside myLocalScope. Run the tests and then follow the instructions commented out in the editor.
+
+Hint
+Refreshing the page may help if you get stuck. */
+
+function myLocalScope() {
+  'use strict';
+// Only change code below this line
+  var myVar = "Test";
+
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+//console.log(myVar);
+
+// Now remove the console log line to pass the test
