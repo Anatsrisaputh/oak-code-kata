0f941660d06cb6b43a7fc36e006b4f559224bec2
Anatsrisaputh <jane.stbc@gmail.com>
2020-05-27 22:56:11 +0700

    Update homework

--- javascript/access-array-data-with-indexes.js
+++ javascript/access-array-data-with-indexes.js
@@ -9,18 +9,4 @@
 // var myData = myArray[0];
 // console.log(myData);
 
-function testGreaterOrEqual(val) {
-  if (val >= 20) {  // Change this line
-    return "20 or Over";
-  
-  }
-
-  if (val >= 10) {  // Change this line
-    return "10 or Over";
-  }
-
-  return "Less than 10";
-}
-
-testGreaterOrEqual(10);
 
