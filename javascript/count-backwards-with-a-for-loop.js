4ee968bc287d26af2032de4ef0e73403dc96c8d4
Anatsrisaputh <jane.stbc@gmail.com>
2020-05-29 23:25:40 +0700

    Update

---
+++ javascript/count-backwards-with-a-for-loop.js
@@ -0,0 +1,7 @@
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+var myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line
+console.log(myArray);
---
+++ javascript/iterate-with-javascript-for-loops.js
@@ -0,0 +1,7 @@
+// Use a for loop to work to push the values 1 through 5 onto myArray.
+var myArray = [];
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+// Only change code below this line
+console.log(myArray);
