d3acd1992ffb047befd317f631a5169a43025f8a
Anatsrisaputh <jane.stbc@gmail.com>
2020-06-12 23:09:55 +0700

    update homework

---
+++ javascript/generate-random-fractions-with-javascript.js
@@ -0,0 +1,9 @@
+function randomFraction() {
+let result = 0;
+  // Only change code below this line
+while (result === 0) {
+  return result = Math.random();
+}
+return result;
+  // Only change code above this line
+}
